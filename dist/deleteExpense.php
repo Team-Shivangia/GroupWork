81b4128008bb2a56a8df6190d78fe08f56e73aee
BoaTenG0 <gboateng.ac@st.vvu.edu.gh>
2023-12-13 12:09:10 +0000

    completed function to delete expense

--- dist/deleteExpense.php
+++ dist/deleteExpense.php
@@ -48,7 +48,33 @@ if (isset($_GET['deletid'])) {
                 } else {
                     die(mysqli_error($con));
                 }
-            } 
+            } elseif ($type === 'expense') {
+                // Fetch the user's current balance and expense
+                $fetch_balance_query = "SELECT `balance`, `expense` FROM `account` WHERE `user_id` = '$user_id'";
+                $balance_result = mysqli_query($con, $fetch_balance_query);
+
+                if ($balance_result) {
+                    $balance_data = mysqli_fetch_assoc($balance_result);
+                    $current_balance = $balance_data['balance'];
+                    $current_expense = $balance_data['expense'];
+
+                    // Update the user's balance by adding the amount
+                    $new_balance = $current_balance + $amount;
+
+                    // Update the user's expense by subtracting the amount
+                    $new_expense = $current_expense - $amount;
+
+                    // Update the user's balance and expense
+                    $update_balance_query = "UPDATE `account` SET `balance` = $new_balance, `expense` = $new_expense WHERE `user_id` = '$user_id'";
+                    $update_balance_result = mysqli_query($con, $update_balance_query);
+
+                    if (!$update_balance_result) {
+                        die(mysqli_error($con));
+                    }
+                } else {
+                    die(mysqli_error($con));
+                }
+            }
 
             header('location:dash.php');
         } else {
